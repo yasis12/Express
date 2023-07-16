af4f1af206c56e90bf153690c1e00ba1ab95e88b
Rafael Yasis <rafaelyasis@gmail.com>
2023-07-16 10:51:15 -0500

    Followed along with lecture but ran into some problems

---
+++ server/server.js
@@ -0,0 +1,14 @@
+// Require express - gives us a function of express
+const express = require('express');
+
+// Create an instance of eexpress by calling thge gunction retuned above - gives us an object
+const app = express();
+const port = 5001    
+
+// express static file serving - public is the folder name
+app.use(express.static('server/public'));
+
+// starts up our server
+app.listen(post, () => {
+    console.log('Listening on port', port);
+})
